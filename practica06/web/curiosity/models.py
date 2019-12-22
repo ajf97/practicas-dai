a355b2cdc10e957919cf50d573f564a2ebdc9a98
Alejandro Jerónimo <ajf97@correo.ugr.es>
2019-12-22 12:52:30 +0100

    Añadidos modelos

--- practica06/web/curiosity/models.py
+++ practica06/web/curiosity/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Musician(models.Model):
+    name = models.CharField(max_length=35)
+    birth_date = models.DateField(blank=False, null=True)
+    instrument = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=20)
+    record_company = models.CharField(max_length=25)
+    release_date = models.DateField(blank=False, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class MusicalGroup(models.Model):
+    name = models.CharField(max_length=35)
+    created_date = models.DateField(blank=True, null=True)
+    style = models.CharField(max_length=20)
+    musicians = models.ManyToManyField(Musician)
+    albums = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
