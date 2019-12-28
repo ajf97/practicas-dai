19d66ebc2b412b627d62e9817aaccdaa5492bdda
Alejandro Jerónimo <ajf97@correo.ugr.es>
2019-12-28 17:40:12 +0100

    Preparación P7

---
+++ practica07/web/curiosity/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from .models import MusicalGroup, Album, Musician
+from django.contrib.auth.models import User
+
+class MusicalGroupForm(forms.ModelForm):
+    class Meta:
+        model = MusicalGroup
+        fields = ('name', 'created_date', 'style', 'musicians', 'albums')
+
+
+class AlbumForm(forms.ModelForm):
+    class Meta:
+        model = Album
+        fields = ('title', 'record_company', 'release_date')
+
+
+class MusicianForm(forms.ModelForm):
+    class Meta:
+        model = Musician
+        fields = ('name', 'birth_date', 'instrument')
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
